4e07b277fe345edf0523f68a002e43ed7134a967
changhaonan <chnme40cs@gmail.com>
2022-11-19 12:12:32 -0500

    integrate the simulation

--- examples/mono_star/opt/PenaltyConstants.cu
+++ examples/mono_star/opt/PenaltyConstants.cu
@@ -7,27 +7,27 @@ star::PenaltyConstants::PenaltyConstants()
 
 void star::PenaltyConstants::setDefaultValue()
 {
-	m_lambda_dense_image_depth = 1.0f;
-	m_lambda_dense_image_optical_flow = 1.0f;
-	m_lambda_reg = 3.0f; // 2.3f for k4, 1.0 for k8
+	m_lambda_dense_image_depth = 0.1f;
+	m_lambda_dense_image_optical_flow = 0.1f;
+	m_lambda_reg = 0.3f; // 2.3f for k4, 1.0 for k8
 	m_lambda_node_translation = 0.0f;
 	m_lambda_feature = 0.f;
 }
 
 void star::PenaltyConstants::setGlobalIterationValue()
 {
-	m_lambda_dense_image_depth = 1.0f;
-	m_lambda_dense_image_optical_flow = 1.0f;
-	m_lambda_reg = 3.0f; // Global iteration has smaller regulation (0.3)
+	m_lambda_dense_image_depth = 0.1f;
+	m_lambda_dense_image_optical_flow = 0.1f;
+	m_lambda_reg = 0.3f; // Global iteration has smaller regulation (0.3)
 	m_lambda_node_translation = 0.0f;
 	m_lambda_feature = 0.f;
 }
 
 void star::PenaltyConstants::setLocalIterationValue()
 {
-	m_lambda_dense_image_depth = 1.0f;
-	m_lambda_dense_image_optical_flow = 1.0f;
-	m_lambda_reg = 3.0f; // Local iteration has larger regulation (0.3)
+	m_lambda_dense_image_depth = 0.1f;
+	m_lambda_dense_image_optical_flow = 0.1f;
+	m_lambda_reg = 0.3f; // Local iteration has larger regulation (0.3)
 	m_lambda_node_translation = 0.0f;
 	m_lambda_feature = 0.f;
 }
