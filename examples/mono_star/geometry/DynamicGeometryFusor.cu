d467bd082d33b02e00d21d4761269e4bec22455f
changhaonan <chnme40cs@gmail.com>
2022-12-09 14:13:59 -0500

    update: found error

--- examples/mono_star/geometry/DynamicGeometryFusor.cu
+++ examples/mono_star/geometry/DynamicGeometryFusor.cu
@@ -2,6 +2,143 @@
 #include <star/geometry/node_graph/NodeGraphManipulator.h>
 #include <star/geometry/node_graph/Skinner.h>
 
+star::DynamicGeometryFusor::DynamicGeometryFusor(
+    SurfelGeometry::Ptr model_surfel_geometry[2],
+    NodeGraph::Ptr node_graph[2],
+    Renderer::Ptr renderer)
+    : m_active_buffer_idx(0), m_fusion_maps_mapped(false), m_geometry_counter(10)
+{
+    // Binding at initalization
+    m_model_surfel_geometry[0] = model_surfel_geometry[0];
+    m_model_surfel_geometry[1] = model_surfel_geometry[1];
+    m_node_graph[0] = node_graph[0];
+    m_node_graph[1] = node_graph[1];
+    m_renderer = renderer;
+
+    auto &config = ConfigParser::Instance();
+    m_num_cam = config.num_cam();
+    m_dynamic_regulation = config.dynamic_regulation();
+
+    // Camera related
+    unsigned img_cols[d_max_cam];
+    unsigned img_rows[d_max_cam];
+    for (auto cam_idx = 0; cam_idx < m_num_cam; ++cam_idx)
+    {
+        img_cols[cam_idx] = config.downsample_img_cols(cam_idx);
+        img_rows[cam_idx] = config.downsample_img_rows(cam_idx);
+        m_cam2world[cam_idx] = config.extrinsic()[cam_idx];
+        m_intrinsic[cam_idx] = config.rgb_intrinsic_downsample(cam_idx);
+    }
+    m_surfel_fusion_handler = std::make_shared<SurfelFusionHandler>(
+        config.num_cam(),
+        img_cols,
+        img_rows,
+        config.enable_semantic_surfel());
+    m_fusion_remaining_surfel_marker = std::make_shared<FusionRemainingSurfelMarker>(config.num_cam());
+    m_geometry_compact_handler = std::make_shared<GeometryCompactHandler>();
+    m_geometry_append_handler = std::make_shared<DynamicGeometryAppendHandler>();
+}
+
+star::DynamicGeometryFusor::~DynamicGeometryFusor()
+{
+    if (m_fusion_maps_mapped)
+        m_renderer->UnmapFusionMapsFromCuda();
+}
+
+void star::DynamicGeometryFusor::Fuse(
+    const unsigned active_buffer_idx,
+    const unsigned frame_idx,
+    const SurfelMap& surfel_map,
+    SurfelGeometry::Ptr measure_surfel_geometry,
+    cudaStream_t stream)
+{
+    m_active_buffer_idx = active_buffer_idx;
+    // Binding data
+    m_data_surfel_geometry = measure_surfel_geometry;
+
+    // Run the fusion
+    // Only support mono camera for now
+    STAR_CHECK_EQ(m_num_cam, 1);
+    Measure4Fusion measure4fusion{};
+    measure4fusion.vertex_confid_map[0] = surfel_map.VertexConfidReadOnly();
+    measure4fusion.normal_radius_map[0] = surfel_map.NormalRadiusReadOnly();
+    measure4fusion.color_time_map[0] = surfel_map.ColorTimeReadOnly();
+    measure4fusion.index_map[0] = surfel_map.IndexReadOnly();
+    measure4fusion.num_valid_surfel = surfel_map.NumValidSurfels();
+
+    Segmentation4SemanticFusion segmentation4semantic_fusion{};
+    segmentation4semantic_fusion.segmentation[0] = surfel_map.SegmentationReadOnly();
+
+    Measure4GeometryRemoval measure4gemetry_removal{};
+    measure4gemetry_removal.depth4removal_map[0] = surfel_map.DepthReadOnly();
+
+    unsigned current_geometry_idx = m_active_buffer_idx; // Pointer de-coupling
+    unsigned current_node_graph_idx = m_active_buffer_idx;
+    bool geometry_reinit = static_cast<bool>(m_geometry_counter.count());
+
+    // Remove geometry
+    if (geometry_reinit)
+    {
+        geometryRemovalSurfelWarp(
+            measure4gemetry_removal,
+            frame_idx,
+            current_geometry_idx,
+            current_node_graph_idx,
+            stream);
+    }
+    unsigned num_surfel_after_removel = m_model_surfel_geometry[current_geometry_idx]->NumValidSurfels();
+
+    // Fusion (Optional, semantic)
+    Geometry4GeometryAppend geometry4geometry_append;
+    geometryFusion(
+        measure4fusion,
+        segmentation4semantic_fusion,
+        geometry4geometry_append,
+        frame_idx,
+        current_geometry_idx,
+        m_use_semantic,
+        geometry_reinit,
+        stream);
+
+    // Append geometry
+    if (geometry_reinit)
+    {
+        geometryAppend(
+            geometry4geometry_append,
+            frame_idx,
+            current_geometry_idx,
+            current_node_graph_idx,
+            stream);
+    }
+
+    // Skinning update (Using a naive skinning for now)
+    if (geometry_reinit)
+    {
+        const bool is_incremental = true;
+        geometrySkinning(
+            is_incremental,
+            num_surfel_after_removel,
+            current_geometry_idx,
+            current_node_graph_idx,
+            stream);
+    }
+
+    // thread-barrier
+    cudaSafeCall(cudaStreamSynchronize(stream));
+
+    // Pointer-merging
+    if (current_geometry_idx != m_active_buffer_idx)
+    {
+        std::cout << "[DG] Geometry switching!" << std::endl;
+        m_model_surfel_geometry[m_active_buffer_idx].swap(m_model_surfel_geometry[current_geometry_idx]);
+    }
+    if (current_node_graph_idx != m_active_buffer_idx)
+    {
+        std::cout << "[DG] NodeGraph switching!" << std::endl;
+        m_node_graph[m_active_buffer_idx].swap(m_node_graph[current_node_graph_idx]);
+    }
+}
+
 void star::DynamicGeometryFusor::drawFusionMaps(
     const unsigned frame_idx,
     const unsigned geometry_idx,
